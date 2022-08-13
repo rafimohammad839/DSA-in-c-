1b2451faf140bbd463f4c89a7e4d80957bd3299c
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-13 13:45:58 +0530

    Leetcode 198: House Robber Optimized

--- leetcode/198_House_Robber.cpp
+++ leetcode/198_House_Robber.cpp
@@ -2,16 +2,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// ------------------------------------------------ Solution 1 | Space - O(N) | Time - O(N)
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//       int dp[nums.size() + 1];
+//       dp[0] = 0;
+//       dp[1] = nums[0];
+//       for (int i = 1; i < nums.size(); i++) {
+//         dp[i + 1] = max(dp[i], dp[i - 1] + nums[i]);
+//       }
+//       return dp[nums.size()];
+//     }
+// };
+
+// ------------------------------------------------ Solution 2 | Space - O(1) | Time - O(N)
 class Solution {
 public:
     int rob(vector<int>& nums) {
-      int dp[nums.size() + 1];
-      dp[0] = 0;
-      dp[1] = nums[0];
+      int first = 0, second = nums[0];
       for (int i = 1; i < nums.size(); i++) {
-        dp[i + 1] = max(dp[i], dp[i - 1] + nums[i]);
+        int temp = max(second, first + nums[i]);
+        first = second;
+        second = temp;
       }
-      return dp[nums.size()];
+      return second;
     }
 };
 
