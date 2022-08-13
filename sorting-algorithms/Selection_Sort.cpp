5b5ad131bb599bbe5180081e979dd476c39413d4
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-13 15:24:53 +0530

    Selection Sort

---
+++ sorting-algorithms/Selection_Sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[]) {
+  for (int i = 0; i < 4; i++) {
+    int select = i;
+    for (int j = i + 1; j < 5; j++) {
+      if (arr[j] <= arr[select]) {
+        select = j;
+      }
+    }
+    swap(arr[i], arr[select]);
+  }
+}
+
+int main()
+{
+  int arr[5] = {10, 3, 20, 50, 9};
+  selectionSort(arr);
+  for (auto ele : arr) {
+    cout << ele << " ";
+  }
+  cout << endl;
+  return 0;
+}
