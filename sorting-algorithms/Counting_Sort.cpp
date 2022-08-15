5f744a96f2bbd44c8555dae6c2293d90ae5f0ef8
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-15 19:28:33 +0530

    Counting Sort

---
+++ sorting-algorithms/Counting_Sort.cpp
@@ -0,0 +1,40 @@
+// ------------------------------------------ Counting Sort | Time - O(N+k) | Auxiliary Space - O(N+k)
+#include <iostream>
+using namespace std;
+
+void countingSort(int arr[], int n) {
+  int k = arr[0];
+
+  for (int i = 0; i < n; i++) {
+    k = max(k, arr[i]);
+  }
+
+  int count[k] = {0};
+  for (int i = 0; i < n; i++) {
+    count[arr[i]]++;
+  }
+
+  for (int i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+
+  int sortedArr[n];
+  for (int i = n - 1; i >= 0; i--) {
+    sortedArr[--count[arr[i]]] = arr[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    arr[i] = sortedArr[i];
+  }
+}
+
+int main() {
+  cout << "Counting Sort!\n";
+  int arr[] = {6, 3, 2, 2, 1, 1, 1, 4, 5};
+  countingSort(arr, 9);
+  cout << "After sorting:\n";
+  for (int i = 0; i < 9; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
